f1942924a3ba0f5098980d911d987144ece980e7
Masao <masao8231>
2021-11-29 22:17:06 -0400

    basic tileset code

--- main.lua
+++ main.lua
@@ -3,6 +3,18 @@ require("engine.sprite")
 require("engine.2dobject")
 require("engine.player")
 
+map = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1},
+}
+
+tiles = {}
+
 function love.load()
     local p = Vector2(2,4)
     local normalp = p:normalize()
@@ -11,6 +23,20 @@ function love.load()
 
     sprite = Sprite(200, 100, love.graphics.newImage("assets/placeholders/sprites/brick.png"))
     player = Player(10, 10, love.graphics.newImage("assets/placeholders/sprites/anim_cycle.png"), 42, 74)
+    tile = love.graphics.newImage("assets/placeholders/tile/tile.png")
+    
+    --loading tiles
+    line = 0
+    for y = 1, #map do
+        line = line + 1
+        for x=1, #map[y] do 
+            if (map[y][x] == 1) then 
+                table.insert(tiles, Vector2((x * tile:getWidth()) - tile:getWidth(), (y * tile:getHeight()) - tile:getHeight()))
+            end 
+        end 
+    end
+    print(row)
+
 end
 
 function love.update(dt)
@@ -20,4 +46,9 @@ end
 function love.draw()  
     sprite:draw()
     player:draw()
+
+    --draw the tiles 
+    for i=1, #tiles do 
+        love.graphics.draw(tile, tiles[i].x, tiles[i].y)
+    end
 end
--- todo.md
+++ todo.md
@@ -2,6 +2,7 @@
  1. Animation Module
     1. Implement on the player module
  2. Tiles 
+    1. Fix the mess 
  3. Player movement 
     1. Apply Basics Physics 
  4. Physics 
